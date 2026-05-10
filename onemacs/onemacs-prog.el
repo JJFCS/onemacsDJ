210f983f2e47cdeea5975a25ce9bfd89f2b09dc7
onncera <onncera@localhost.com>
2026-05-10 22:14:38 +0800

    simple lsp setup using eglot

--- onemacs/onemacs-prog.el
+++ onemacs/onemacs-prog.el
@@ -6,6 +6,30 @@
 	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+	;; a very simple LSP setup (for now perhaps..)
+
+	(use-package eglot
+		:hook (
+				(python-mode . eglot-ensure)
+				(c-mode      . eglot-ensure)
+		)
+
+		:config	
+
+		(add-to-list 'eglot-server-programs
+					 '(python-mode . ("basedpyright-langserver" "--stdio")
+		)
+		)
+
+		(add-to-list 'eglot-server-programs
+					 '(c-mode      . ("clangd")
+		)
+		)
+	)
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	(use-package dumb-jump  :ensure t)
 	(use-package devdocs    :ensure t
 		:custom (devdocs-data-dir (expand-file-name "onemacs-cache/devdocs" user-emacs-directory)
