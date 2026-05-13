7f9f5be63485f28355d9af6f1620ecdcf0c38f62
onncera <onncera@localhost.com>
2026-05-13 14:12:34 +0800

    maybe in the future we want posframe like telescope from neovim

--- onemacs/onemacs-completions.el
+++ onemacs/onemacs-completions.el
@@ -43,6 +43,107 @@
 			("C-c s" . vertico-grid-mode)
 				)
 			)
+
+
+;; 	;; If you ever want to go for that telescope look in neovim where the
+;; 	;; popup is in the middle of the screen. Play around with the code below.
+;; 	;; Can make use of asking AI as well to polish the code.
+
+
+;; 	(use-package vertico :ensure t :init (vertico-mode) (vertico-multiform-mode) :config
+;; 		(require 'vertico-multiform)
+;; 		(setq vertico-multiform-commands '(
+;; 			(consult-find      buffer)
+;; 			(consult-grep      buffer)
+;; 			(consult-line      buffer)
+;; 			(onncera-root-find buffer)
+;; 			(onncera-root-grep buffer)
+;; 			(imenu buffer)
+;; 				)
+;; 			)
+;; 		(require 'vertico-quick)
+;; 			:bind (:map vertico-map
+;; 			("C-q"   . vertico-quick-insert) ("M-q" . vertico-quick-exit)
+;; 			("C-c s" . vertico-grid-mode)
+;; 				)
+
+;; 		:custom
+;; 			(vertico-resize t)  ;; allows dynamic resizing
+;; 			(vertico-cycle  t)  ;; cycle candidates
+;; 			(vertico-count 20)  ;; The number of visible candidates
+		
+;; 			)
+
+
+;; 	;; ----------------------------
+;; 	;; Posframe
+;; 	;; floating child frames
+;; 	;; ----------------------------
+;; 	(use-package posframe :ensure t)
+
+;; 	;; ----------------------------
+;; 	;; Vertico Posframe
+;; 	;; centered floating minibuffer
+;; 	;; ----------------------------
+;; 	(use-package vertico-posframe :ensure t :after vertico
+
+;; 		:custom
+
+;; 		;; Fallback if childframes unsupported
+;; 		(vertico-posframe-fallback-mode 'vertico-buffer-mode)
+
+;; 		;; Dimensions
+;; 		(vertico-posframe-width 120)
+;; 		(vertico-posframe-height 16)
+
+;; 		;; Prevent weird shrinking
+;; 		(vertico-posframe-min-width 120)
+;; 		(vertico-posframe-min-height 16)
+
+;; 		;; center on screen
+;; 		(vertico-posframe-poshandler #'posframe-poshandler-frame-center)
+
+;; 		;; Border
+;; 		(vertico-posframe-border-width 1)
+
+;; 		:config
+;; 		(vertico-posframe-mode 1)
+
+;; 	)
+
+;; 	;; ----------------------------
+;; 	;; Telescope-like appearance
+;; 	;; ----------------------------
+
+;; (custom-set-faces
+;; 	'(vertico-posframe
+;; 		((t (:background "#1e1e2e"
+;; 		     :foreground "#cdd6f4"))))
+
+;; 	'(vertico-posframe-border
+;; 		((t (:foreground "#6c7086"))))
+
+;; 	'(vertico-current
+;; 		((t (:background "#313244"
+;; 			 :foreground "#f5e0dc"
+;; 			 :weight bold))))
+
+;; 	'(marginalia-documentation
+;; 		((t (:foreground "#7f849c"))))
+
+;; 	'(minibuffer-prompt
+;; 		((t (:foreground "#89b4fa"
+;; 			 :weight bold)))))
+
+;; 	;; ----------------------------
+;; 	;; optional:
+;; 	;; slightly higher popup uncomment if desired
+;; 	;; ----------------------------
+
+;; 	;; (setq vertico-posframe-poshandler
+;; 	;;       #'posframe-poshandler-frame-center-near-top)
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
