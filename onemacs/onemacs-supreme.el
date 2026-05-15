409cb2dde128d6b9645712ce2f574527fa13e265
onncera <onncera@localhost.com>
2026-05-15 08:30:26 +0800

    moved os to supreme & included diff hl package

--- onemacs/onemacs-supreme.el
+++ onemacs/onemacs-supreme.el
@@ -2,7 +2,26 @@
 	;; -*- lexical-binding: t; -*-
 
 	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	(use-package magit :ensure t :defer t)
+	(setq locate-command "mdfind")  ;; only for macOS - use spotlight for locate
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	;; In terminal mode , keep long lines on one line
+	(add-hook 'term-mode-hook (lambda () (setq-local truncate-lines t)))
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	(use-package exec-path-from-shell
+	:ensure t
+	:if (and (display-graphic-p)
+		(eq system-type 'darwin))
+	:demand t
+	:config
+	(setq exec-path-from-shell-variables '("PATH"))
+	(exec-path-from-shell-initialize)
+	)
 	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
