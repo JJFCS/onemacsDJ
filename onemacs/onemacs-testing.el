1bf0fa0ab9954e234ad7fc50d1d634bac1a68571
onncera <onncera@localhost.com>
2026-05-19 00:50:27 +0800

    testing org mode things

--- onemacs/onemacs-testing.el
+++ onemacs/onemacs-testing.el
@@ -2,7 +2,32 @@
 	;; -*- lexical-binding: t; -*-
 
 	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	;; Type here..
+	;; testing org mode things
+	(use-package olivetti
+		:ensure t
+		:hook (org-mode . olivetti-mode)
+		:config
+		;; The "Nuclear Option": Force the reset function to be safe
+		;; This overwrites the broken internal calculation with a safe 'nil' (default)
+		(defun olivetti-reset-window (window)
+			(when (window-live-p window)
+				(set-window-margins window nil)
+				(set-window-fringes window nil nil)
+				(set-window-parameter window 'split-window nil)
+			)
+		)
+	)
+
+	(use-package org-bullets :ensure t :hook (org-mode . org-bullets-mode))
+
+	(with-eval-after-load 'org
+		(set-face-attribute 'org-level-1 nil :weight 'bold :height 1.00)
+		(set-face-attribute 'org-level-2 nil :weight 'bold :height 1.00)
+		(set-face-attribute 'org-level-3 nil :weight 'bold :height 1.00)
+		(set-face-attribute 'org-level-4 nil :weight 'bold :height 1.00)
+	)
+
+	(add-hook 'org-mode-hook 'visual-line-mode)
 	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
