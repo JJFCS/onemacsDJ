8a6321930f19f407c582d95c49da15b2179bcc28
onncera <onncera@localhost.com>
2026-06-18 23:48:47 +0800

    spring cleaning & change default theme

--- init.el
+++ init.el
@@ -41,11 +41,11 @@
 	;; - Iosevka Nerd Font Mono
 	(add-hook 'after-init-hook
 		(lambda ()
-		        (set-face-attribute 'default nil        :family "MartianMono Nerd Font Mono" :height 140 :width 'condensed :weight 'regular :slant 'normal)
+			(set-face-attribute 'default nil        :family "MartianMono Nerd Font Mono" :height 140 :width 'condensed :weight 'regular :slant 'normal)
 			(set-face-attribute 'fixed-pitch nil    :family "MartianMono Nerd Font Mono" :height 140 :width 'condensed :weight 'regular :slant 'normal)
 			(set-face-attribute 'variable-pitch nil :family "Merriweather" :height 140)
-		        (set-face-attribute 'completions-annotations nil :slant 'normal)
-        		(set-face-attribute 'completions-common-part nil :slant 'normal)
+			(set-face-attribute 'completions-annotations nil :slant 'normal)
+			(set-face-attribute 'completions-common-part nil :slant 'normal)
 		)
 	)
 	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
--- onemacs/onemacs-supreme.el
+++ onemacs/onemacs-supreme.el
@@ -34,7 +34,7 @@
 
 		(add-hook 'window-setup-hook	(lambda ()
 		(run-with-idle-timer 0 nil	(lambda ()
-							(load-theme 'pixel-themes-alia16 t)
+							(load-theme 'turbo-c t)
 
 						)
 		)
