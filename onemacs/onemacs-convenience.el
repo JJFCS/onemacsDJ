0f4903810b3f5dfaa56abad51ef41d6b71cbb3ba
onncera <onncera@localhost.com>
2026-06-22 00:16:57 +0800

    which key fix

--- onemacs/onemacs-convenience.el
+++ onemacs/onemacs-convenience.el
@@ -28,11 +28,13 @@
 
 
 	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	(use-package which-key :init (which-key-mode)
-		:config
-		(setq which-key-show-early-on-C-h t)
-		(setq which-key-idle-delay 1e6)
-		(setq which-key-idle-secondary-delay 0.05)
+	(use-package which-key
+	:init
+	(setq which-key-show-early-on-C-h t)
+	(setq which-key-idle-delay 1e6)
+	(setq which-key-idle-secondary-delay 0.05)
+	:config
+	(which-key-mode)
 	)
 	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
