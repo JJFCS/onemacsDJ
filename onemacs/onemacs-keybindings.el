ed1af21673d89f0588bbd3af42899e89a567c0b4
onncera <onncera@localhost.com>
2026-06-28 23:27:03 +0800

    included avy & keybinds

--- onemacs/onemacs-keybindings.el
+++ onemacs/onemacs-keybindings.el
@@ -19,9 +19,12 @@
 	;; TODO - setup keybindings..
 	(defvar onncera/a-map (make-sparse-keymap) "onncera C-c a prefix")
 	(global-set-key (kbd "C-c a") onncera/a-map)
-	(define-key onncera/a-map (kbd "a") #'completion-at-point)  ;; corfu provides the UI, command is builtin
-	(define-key onncera/a-map (kbd "b") #'onncera-standard-tab)
-	(define-key onncera/a-map (kbd "c") #'view-lossage)
+	(define-key onncera/a-map (kbd "a") #'avy-goto-char)
+	(define-key onncera/a-map (kbd "c") #'completion-at-point)  ;; corfu provides the UI, command is builtin
+	(define-key onncera/a-map (kbd "h") #'view-lossage)
+	(define-key onncera/a-map (kbd "q") #'onncera-standard-tab)
+	(define-key onncera/a-map (kbd "s") #'avy-goto-line)
+	(define-key onncera/a-map (kbd "t") #'ansi-term)
 
 	;; example for non builtin commands
 	;; b - (reserved, example only, uncomment + fill in when needed)
